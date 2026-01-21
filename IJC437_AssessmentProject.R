02b8df245e8742252e856a79c585a9787feb8d04
Pavithra Subramanian <mail.s.pavithra@gmail.com>
2026-01-21 18:29:57 +0000

    Updated Plots to uniform colors

--- IJC437_AssessmentProject.R
+++ IJC437_AssessmentProject.R
@@ -3,6 +3,7 @@ install.packages(("patchwork"))
 install.packages("reshape2")
 install.packages("fpc") 
 install.packages("clusterCrit")
+install.packages("psych")
 library(tidyverse)
 library(MASS)
 library(rgl)
@@ -11,6 +12,8 @@ library(reshape2)
 library(fpc)
 library(clusterCrit)
 library(cluster)
+library(psych)
+library(RColorBrewer)
 
 
 
@@ -22,6 +25,8 @@ ncol(df1)
 
 head(df1)
 tail(df1)
+
+describe(df1)
 
 # Create a categorical variable Rank group:
 df1 <-   df1 %>% 
@@ -53,7 +58,7 @@ corr_df <- melt(corr_mat, varnames = c("Var1", "Var2"), value.name = "cor")
 #plotting the correlation matrix:
 ggplot(corr_df, aes(Var1, Var2, fill = cor)) +
   geom_tile(colour="black") +
-  scale_fill_gradient2(low="black", high="pink",limits = c(-1, 1)) +
+  scale_fill_gradient2(low="lightblue", high="darkblue",limits = c(-1, 1)) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   labs(title = "Correlation matrix")+
@@ -80,6 +85,7 @@ n_clusters <- 20
 # Initialize total within sum of squares error: wss
 wss <- numeric(n_clusters)
 sil_widths <- numeric(n_clusters)
+sil_widthspca<-numeric(n_clusters)
 set.seed(123)
 
 #Clustering on Dataset without PCA:
@@ -97,6 +103,7 @@ for (i in 1:n_clusters) {
   }
 }
 
+#Plotting Silhouette widths for all Clusters:
 
 sil_df <- data.frame(
   k = 1:n_clusters,
@@ -104,15 +111,38 @@ sil_df <- data.frame(
 )
 
 ggplot(sil_df, aes(x = k, y = sil_width)) +
-  geom_line(color = "darkred", size = 1.2) +
-  geom_point(color = "darkred", size = 3) +
+  geom_line(color = "purple", size = 1.2) +
+  geom_point(color = "darkblue", size = 3) +
   labs(
     x = "Number of clusters (k)",
     y = "Average silhouette width",
-    title = "Silhouette analysis on PCA-transformed data"
+    title = "Silhouette analysis on Original data"
   ) +
   theme_minimal()
-#Clustering on Dataset after PCA:
+
+
+# Plotting WSS for all cluster values
+
+wss_df <- tibble(clusters = 1:n_clusters, wss = wss)
+
+scree_plot <- ggplot(wss_df, aes(x = clusters, y = wss, group = 1)) +
+  geom_line(color = "purple", size = 1.2) +
+  geom_point(color = "darkblue", size = 3) +
+  geom_line() +
+  labs(
+    x = "Number of clusters (k)",
+    y = "WSS",
+    title = "WSS analysis on Original data"
+  ) +
+  theme_minimal()
+
+scree_plot +geom_hline(
+  yintercept = wss, 
+  linetype = 'dashed')
+
+
+
+##Clustering on Dataset after PCA:
 # Look over 1 to n possible clusters
 wss_pca <- numeric(n_clusters)
 for (i in 1:n_clusters) {
@@ -120,107 +150,104 @@ for (i in 1:n_clusters) {
   km.out.pca <- kmeans(scaledpca_df, centers = i, nstart = 20, iter.max=20)
   # Save the within cluster sum of squares
   wss_pca[i] <- km.out.pca$tot.withinss
+  sil_pca <- silhouette(km.out.pca$cluster, dist(scaledpca_df))
+  if (!is.na(sil_pca[1])) {
+    sil_widthspca[i] <- mean(sil_pca[, 3])  # 3rd column = sil_width
+  } else {
+    sil_widthspca[i] <- NA  # k=1 always gives NA silhouette
+  }
 }
+
+#Plotting Silhouette widths for all Clusters:
+sil_df_pca <- data.frame(
+  k = 1:n_clusters,
+  sil_widthpca = sil_widthspca
+)
+#Plotting the Sihouette widths for the Clusters:
+ggplot(sil_df_pca, aes(x = k, y = sil_widthpca)) +
+  geom_line(color = "purple", size = 1.2) +
+  geom_point(color = "darkblue", size = 3) +
+  labs(
+    x = "Number of clusters (k)",
+    y = "Average silhouette width",
+    title = "Silhouette analysis on PCA-transformed data"
+  ) +
+  theme_minimal()
+
 
 # Plotting WSS for all cluster values
 
-#Dataset without PCA:
-wss_df <- tibble(clusters = 1:n_clusters, wss = wss)
-
-scree_plot <- ggplot(wss_df, aes(x = clusters, y = wss, group = 1)) +
-  geom_point(size = 4)+
-  geom_line() +
-    xlab('Number of clusters')
-
-scree_plot +geom_hline(
-  yintercept = wss, 
-  linetype = 'dashed',  )
-
-#Dataset with PCA:
 wss_df_pca <- tibble(clusters = 1:n_clusters, wss = wss_pca)
 
 scree_plot <- ggplot(wss_df_pca, aes(x = clusters, y = wss_pca, group = 1)) +
-  geom_point(size = 4)+
+  geom_line(color = "purple", size = 1.2) +
+  geom_point(color = "darkblue", size = 3) +
   geom_line() +
-  xlab('Number of clusters')
+  labs(
+    x = "Number of clusters (k)",
+    y = "WSS",
+    title = "WSS analysis on PCA-Transformed data"
+  ) +
+  theme_minimal()
 
 scree_plot +geom_hline(
   yintercept = wss_pca, 
-  linetype = 'dashed',  )
+  linetype = 'dashed')
+
+
 
 # deciding on Cluster size:
 set.seed(123)
 
-km.out <- kmeans(scaled_df, centers = 10, nstart = 20, iter.max=20)
-km.out.pca <- kmeans(scaledpca_df, centers = 4, nstart = 20, iter.max=20)
+km.out <- kmeans(scaled_df, centers = 8, nstart = 20, iter.max=20)
+km.out.pca <- kmeans(scaledpca_df, centers = 5, nstart = 20, iter.max=20)
 
 #Evaluating the Clusters with the Calinski-Harabasz Index:
 #dataset Without PCA
 ch1 <- calinhara(scaled_df, km.out$cluster)
 ch1
 
-  #dataset with PCA
+#dataset with PCA
 ch2 <- calinhara(scaledpca_df, km.out.pca$cluster)
 ch2
 
 #Evaluating the Clusters with the Dunn Index:
 
-#Dunn Index for dataset without PCA:
-dunn <- intCriteria(as.matrix(scaled_df), km.out$cluster, "Dunn")
-dunn$dunn
-#Dunn Index for dataset with PCA:
-dunn1 <- intCriteria(as.matrix(scaledpca_df), km.out.pca$cluster, "Dunn")
-dunn1$dunn
-
-#Calculating RMSE for the clusters:
-#RMSE for Dataset without PCA:
-centers     <- km.out$centers[km.out$cluster, , drop = FALSE]
-sq_dist     <- rowSums((scaled_df - centers)^2)   # squared Euclidean distances
-rmse_clust  <- sqrt(mean(sq_dist))
-rmse_clust
-
-#RMSE for Dataset with PCA:
-centers_pca     <- km.out.pca$centers[km.out.pca$cluster, , drop = FALSE]
-sq_dist_pca     <- rowSums((scaledpca_df - centers_pca)^2)   # squared Euclidean distances
-rmse_clust_pca  <- sqrt(mean(sq_dist_pca))
-rmse_clust_pca
 
 #Plotting Cluster distribution on the dataset:
 
 #Dataset Without PCA
 df1$cluster_id <- factor(km.out$cluster)
 ggplot(df1, aes(x=cluster_id)) +
-  geom_bar() +
-  xlab("cluster_id")
+  geom_bar(fill="darkblue") +
+  labs(
+    x = "Cluster ID",
+    y = "Count of Songs",
+    title = "Cluster distribution of Original data"
+  ) +
+  theme_minimal()
 
 #Dataset with PCA
 df1$cluster_id_pca <- factor(km.out.pca$cluster)
 ggplot(df1, aes(x=cluster_id_pca)) +
-  geom_bar() +
-  xlab("cluster_id after PCA") 
-
+  geom_bar(fill="darkblue") +
+  labs(
+    x = "Cluster ID",
+    y = "Count of Songs",
+    title = "Cluster distribution of PCA Transformed data"
+  ) +
+  theme_minimal()
 
 #Plotting orginal principle components against clusters:
-ggplot(df1, aes(x = pca1, y=pca2, fill = cluster_id_pca)) +
-  geom_boxplot()
-
-
-# Checking if PCA after clustering can give any new insights:
-
-num2 <- subset(df1, select= -c(song_id,max_rank,B_NB,duration_ms,rank_group,key,mode,time_signature,pca1,pca2,cluster_id_pca))
-num2$cluster_id <- as.numeric(as.character(num2$cluster_id))
-
-scaled2<-scale(num2)
-
-#performing PCA and obtaining scores pc1 and pc2
-pca3<-prcomp(scaled2)
-df1$pca_ac1<-pca3$x[,1]
-df1$pca_ac2<-pca3$x[,2]
-
-#Plotting Principle components after clustering to see if new insights emerge:
-ggplot(df1, aes(x = pca_ac1, y=pca_ac2, fill = cluster_id)) +
-  geom_boxplot() 
-
+ggplot(df1, aes(x = pca1, y = pca2, fill = cluster_id_pca)) +
+  geom_boxplot() +
+  scale_fill_brewer(palette = "Blues", direction = 1) +
+  labs(
+    x = "PC 1",
+    y = "PC 2",
+    title = "Distribution of Clusters between Principal components"
+  ) +
+  theme_minimal() 
 
 
 #Plotting Cluster against Billboard ranked songs to see if pattern emerges:
@@ -231,18 +258,22 @@ df_billboard<- df1 |>
 #plotting based on Cluster Id generated with out PCA:
 ggplot(df_billboard, aes(x = cluster_id, fill = rank_group)) +
   geom_bar() +
+  scale_fill_brewer(palette = "Blues", direction = -1) +
   labs(
     x = "Group",
-    fill = "Cluster ID"
+    fill = "Cluster ID",
+    title = "Distribution of Billboard Ranked Songs"
   ) +
   theme_minimal()
 
 #plotting based on Cluster Id generated with PCA:
 ggplot(df_billboard, aes(x = cluster_id_pca, fill = rank_group)) +
   geom_bar() +
+  scale_fill_brewer(palette = "Blues", direction = 1) +
   labs(
     x = "Group",
-    fill = "Cluster ID"
+    fill = "Cluster ID",
+    title = "Distribution of Billboard Ranked Songs"
   ) +
   theme_minimal()
 
